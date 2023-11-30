c2967d95b98e4bc5ca5b05e4315df889ba3d9b24
leighaderango <104590969+leighaderango@users.noreply.github.com>
2023-11-30 18:45:43 +0000

    animated necromancer enemy
    - still needs to be mirrored when going to the left

--- final_project/Assets/TopDownMovement.cs
+++ final_project/Assets/TopDownMovement.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour{
+	
    public float moveSpeed;
-   public Rigidbody2D rb;
+   public Animator animator;
    private Vector2 moveInput;
    
-   void Update(){
+   
+   
+   void FixedUpdate(){
 	   moveInput.x = Input.GetAxisRaw("Horizontal");
 	   moveInput.y = Input.GetAxisRaw("Vertical");
 	   
 	   moveInput.Normalize();
-	   rb.velocity = moveInput * moveSpeed;
+	   GetComponent<Rigidbody2D>().velocity = moveInput * moveSpeed;
+	   
+	   animator.SetFloat("Speed", Mathf.Abs(moveInput.x));
    }
 }
 
