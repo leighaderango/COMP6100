77f7ea612f73f4e20963d37c273258bdd397bbd1
leighaderango <104590969+leighaderango@users.noreply.github.com>
2023-11-30 15:47:38 +0000

    beginning to animate enemies and how to play

---
+++ final_project/Assets/TopDownMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour{
+   public float moveSpeed;
+   public Rigidbody2D rb;
+   private Vector2 moveInput;
+   
+   void Update(){
+	   moveInput.x = Input.GetAxisRaw("Horizontal");
+	   moveInput.y = Input.GetAxisRaw("Vertical");
+	   
+	   moveInput.Normalize();
+	   rb.velocity = moveInput * moveSpeed;
+   }
+}
+
