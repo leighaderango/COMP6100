49da344ff97b8c8a43a775dc735a7b0006526a09
leighaderango <104590969+leighaderango@users.noreply.github.com>
2023-12-14 22:48:24 +0000

    commit

--- final_project/Assets/Scripts/EndGameCollider.cs
+++ final_project/Assets/Scripts/EndGameCollider.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class EndGameCollider : MonoBehaviour
 {
+	public Canvas winGameCanvas;
+	
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
+		winGameCanvas.GetComponent<Canvas>().enabled = true;
         Debug.Log("Game Over");
+		Time.timeScale = 0;
     }
 }
